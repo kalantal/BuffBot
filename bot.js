596a9d56aeac815fa6f3576deafb2d70df95a6ab
Justin <justinjrestivo@gmail.com>
2020-07-18 16:13:57 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,8 +12,8 @@ const client = new Discord.Client();
 //
 
 client.on("message", (message) => {
-  if(message.content === "!BUFFHELP") {
-    message.channel.send("\n **See upcoming buffs:** \n !ALLIANCE-ALL, !HORDE-ALL \n !ZG \n !ONY-A, !ONY-H \n !SF-A, !SF-H \n NEF-A, !NEF-H \n !WCB \n !SUMMON-A, !SUMMON-H \n !DMF-SUMMON-A, !DMF-SUMMON-H \n PORT-A, PORT-H \n LAYER-A, LAYER-H \n \n **Add upcoming buffs:** \n !ADD BUFF LAYER TIME");
+  if(message.content.toUpperCase() == "!BUFFHELP") {
+    message.channel.send("\n **See Upcoming Buffs:** \n <:dmt:733931789243908136> !alliance-all, !horde-all \n <:zg:733931791802564628> !zg \n <:ony:733931792297492490> !ony-a, !ony-h \n <:songflower:733931791643181137> !sf-a, !sf-h \n <:nef:733931791538192415> !nef-a, !nef-h \n <:wcb:733931791567683614> !wcb, !wcb-a \n <:dmf:733931791798370335> !summon-a, !summon-h \n <:dmf:733931791798370335> !dmf-summon-a, !dmf-summon-h \n <:orgport:734106867160055843> !port-a, !port-h \n <:vanish:734106867227164702> !layer-a, !layer-h \n \n **Add Upcoming Buffs:** \n !add buff layer time cost (if any)");
   }
 });
 
@@ -22,7 +22,7 @@ client.on("message", (message) => {
 //
 
 client.on("message", (message) => {
-  if(message.content === "!ADD $Buff $Layer $Time $Cost") {
+  if(message.content.toUpperCase() == "!ADD $Buff $Layer $Time $Cost") {
     message.channel.send("Buff **$Buff** added\n$Layer\n$Time\n$Cost");
   }
 });
@@ -32,8 +32,8 @@ client.on("message", (message) => {
 //
 
 client.on("message", (message) => {
-  if(message.content === "!ZG") {
-    message.channel.send("<:zg:733931791802564628> " + "**Heart of Hakkar**" + " <:zg:733931791802564628> " + "\n**On:** $Layer" + "\n**At:** $Time" + "\n**By:** $Buffer\n");
+  if(message.content.toUpperCase() == "!ZG") {
+    message.channel.send("<:zg:733931791802564628> " + "**Heart of Hakkar**" + "\n**On:** $Layer" + "\n**At:** $Time" + "\n**By:** $Buffer\n");
   }
 });
 
@@ -42,38 +42,38 @@ client.on("message", (message) => {
 //
 
 client.on("message", (message) => {
-  if(message.content === "!ONY-A") {
-    message.channel.send("<:ony:733931792297492490> " + "**Alliance Onyxia Head**" + " <:ony:733931792297492490>" + "\n**On:** $Layer" + "\n**At:** $Time" + "\n**By:** $Buffer");
+  if(message.content.toUpperCase() == "!ONY-A") {
+    message.channel.send("<:ony:733931792297492490> " + "**Alliance Onyxia Head**" + "\n**On:** $Layer" + "\n**At:** $Time" + "\n**By:** $Buffer");
   }
-  if(message.content === "!NEF-A") {
-    message.channel.send("<:nef:733931791538192415> " + "**Alliance Nefarian Head**" + " <:nef:733931791538192415>" + "\n**On:** $Layer" + "\n**At:** $Time" + "\n**By:** $Buffer");
+  if(message.content.toUpperCase() == "!NEF-A") {
+    message.channel.send("<:nef:733931791538192415> " + "**Alliance Nefarian Head**" + "\n**On:** $Layer" + "\n**At:** $Time" + "\n**By:** $Buffer");
   }
-  if(message.content === "!SF-A") {
-    message.channel.send("<:songflower:733931791643181137> " + "**Alliance Song Flower**" + " <:songflower:733931791643181137>" + "\n**On:** $Layer" + "\n**At:** $Time" + "\n**Cost:** $Cost" + "\n**By:** $Buffer");
+  if(message.content.toUpperCase() == "!SF-A") {
+    message.channel.send("<:songflower:733931791643181137> " + "**Alliance Song Flower**" + "\n**On:** $Layer" + "\n**At:** $Time" + "\n**Cost:** $Cost" + "\n**By:** $Buffer");
   }
-  if(message.content === "!WCB-A") {
-    message.channel.send("<:wcb:733931791567683614> " + "**Warcheifs Blessing**" + " <:wcb:733931791567683614>" + "\n**On:** $Layer" + "\n**At:** $Time" + "\n**Cost:** $Cost" + "\n**By:** $Buffer");
+  if(message.content.toUpperCase() == "!WCB-A") {
+    message.channel.send("<:wcb:733931791567683614> " + "**Warcheifs Blessing**" + "\n**On:** $Layer" + "\n**At:** $Time" + "\n**Cost:** $Cost" + "\n**By:** $Buffer");
   }
-  if(message.content === "!DMT-SUMMON-A") {
-    message.channel.send("<:dmt:733931789243908136> " + "**Alliance Dire Maul Tribute**" + " <:dmt:733931789243908136>" + "\n**On:** $Layer" + "\n**By:** $Buffer");
+  if(message.content.toUpperCase() == "!DMT-SUMMON-A") {
+    message.channel.send("<:dmt:733931789243908136> " + "**Alliance Dire Maul Tribute**" + "\n**On:** $Layer" + "\n**By:** $Buffer");
   }
-  if(message.content === "!DMF-SUMMON-A") {
-    message.channel.send("<:dmf:733931791798370335> " + "**Alliance Darkmoon Faire**" + " <:dmf:733931791798370335>" + "\n**On:** $Layer" + "\n**By:** $Buffer");
+  if(message.content.toUpperCase() == "!DMF-SUMMON-A") {
+    message.channel.send("<:dmf:733931791798370335> " + "**Alliance Darkmoon Faire**" + "\n**On:** $Layer" + "\n**By:** $Buffer");
   }
-  if(message.content === "!BB-SUMMON-A") {
-    message.channel.send("<:zg:733931791802564628> " + "**Alliance Booty Bay Summon**" + " <:zg:733931791802564628>" + "\n**On:** $Layer" + "\n**By:** $Buffer");
+  if(message.content.toUpperCase() == "!BB-SUMMON-A") {
+    message.channel.send("<:zg:733931791802564628> " + "**Alliance Booty Bay Summon**" + "\n**On:** $Layer" + "\n**By:** $Buffer");
   }
-  if(message.content === "!SF-SUMMON-A") {
-    message.channel.send("<:songflower:733931791643181137> " + "**Alliance Songflower Summon**" + " <:songflower:733931791643181137>" + "\n**On:** $Layer" + "\n**At:** $Time"+ "\n**Cost:** $Cost" + "\n**By:** $Buffer");
+  if(message.content.toUpperCase() == "!SF-SUMMON-A") {
+    message.channel.send("<:songflower:733931791643181137> " + "**Alliance Songflower Summon**" + "\n**On:** $Layer" + "\n**At:** $Time"+ "\n**Cost:** $Cost" + "\n**By:** $Buffer");
   }
-  if(message.content === "!LAYER-A") {
-    message.channel.send("<:vanish:734106867227164702> " + "**Alliance Layering**" + " <:vanish:734106867227164702>" + "\n**On:** $Layer" + "\n**By:** $Buffer");
+  if(message.content.toUpperCase() == "!LAYER-A") {
+    message.channel.send("<:vanish:734106867227164702> " + "**Alliance Layering**" + "\n**On:** $Layer" + "\n**By:** $Buffer");
   }
-  if(message.content === "!PORT-A") {
-    message.channel.send("<:orgport:734106867160055843> " + "**Alliance Ports**" + " <:orgport:734106867160055843>" + "\n**On:** $Layer"+ "\n**Cost:** $Cost" + "\n**By:** $Buffer");
+  if(message.content.toUpperCase() == "!PORT-A") {
+    message.channel.send("<:orgport:734106867160055843> " + "**Alliance Ports**" + "\n**On:** $Layer"+ "\n**Cost:** $Cost" + "\n**By:** $Buffer");
   }
-  if(message.content === "!ALLIANCE-ALL") {
-    message.channel.send("<:dmt:733931789243908136> " + "**All Upcoming Alliance Buffs**" + " <:dmt:733931789243908136>" + "\n$ONY-A" + "\n$NEF-A" + "\n$SF-A" + "\n$WCB-A" + "\n$DMT-SUMMON-A" + "\n$BB-SUMMON-A" + "\n$SF-SUMMON-A" + "\n$LAYER-A" + "\n$PORT-A" + "\n$ZG");
+  if(message.content.toUpperCase() == "!ALLIANCE-ALL") {
+    message.channel.send("<:dmt:733931789243908136> " + "**All Upcoming Alliance Buffs**" + "\n<:ony:733931792297492490> $ONY-A" + "\n<:nef:733931791538192415> $NEF-A" + "\n<:songflower:733931791643181137> $SF-A" + "\n<:wcb:733931791567683614> $WCB-A" + "\n<:dmt:733931789243908136> $DMT-SUMMON-A" +"\n<:dmf:733931791798370335> $DMF-SUMMON" + "\n<:zg:733931791802564628> $BB-SUMMON-A" + "\n<:songflower:733931791643181137> $SF-SUMMON-A" + "\n<:vanish:734106867227164702> $LAYER-A" + "\n<:orgport:734106867160055843> $PORT-A" + "\n<:zg:733931791802564628> $ZG");
   }
 });
 
@@ -82,38 +82,38 @@ client.on("message", (message) => {
 //
 
 client.on("message", (message) => {
-  if(message.content === "!ONY-H") {
-    message.channel.send("<:ony:733931792297492490> " + "**Horde Onyxia Head**" + " <:ony:733931792297492490>" + "\n**On:** $Layer" + "\n**At:** $Time" + "\n**By:** $Buffer");
+  if(message.content.toUpperCase() == "!ONY-H") {
+    message.channel.send("<:ony:733931792297492490> " + "**Horde Onyxia Head**" + "\n**On:** $Layer" + "\n**At:** $Time" + "\n**By:** $Buffer");
   }
-  if(message.content === "!NEF-H") {
-    message.channel.send("<:nef:733931791538192415> " + "**Horde Nefarian Head**" + " <:nef:733931791538192415>" + "\n**On:** $Layer" + "\n**At:** $Time" + "\n**By:** $Buffer");
+  if(message.content.toUpperCase() == "!NEF-H") {
+    message.channel.send("<:nef:733931791538192415> " + "**Horde Nefarian Head**" + "\n**On:** $Layer" + "\n**At:** $Time" + "\n**By:** $Buffer");
   }
-  if(message.content === "!SF-H") {
-    message.channel.send("<:songflower:733931791643181137> " + "**Horde Song Flower**" + " <:songflower:733931791643181137>" + "\n**On:** $Layer" + "\n**At:** $Time" + "\n**Cost:** $Cost" + "\n**By:** $Buffer");
+  if(message.content.toUpperCase() == "!SF-H") {
+    message.channel.send("<:songflower:733931791643181137> " + "**Horde Song Flower**" + "\n**On:** $Layer" + "\n**At:** $Time" + "\n**Cost:** $Cost" + "\n**By:** $Buffer");
   }
-  if(message.content === "!WCB") {
-    message.channel.send("<:wcb:733931791567683614> " + "**Warcheifs Blessing**" + " <:wcb:733931791567683614>" + "\n**On:** $Layer" + "\n**At:** $Time" + "\n**Cost:** $Cost" +"\n**By:** $Buffer");
+  if(message.content.toUpperCase() == "!WCB") {
+    message.channel.send("<:wcb:733931791567683614> " + "**Warcheifs Blessing**" + "\n**On:** $Layer" + "\n**At:** $Time" + "\n**Cost:** $Cost" +"\n**By:** $Buffer");
   }
-  if(message.content === "!DMT-SUMMON-H") {
-    message.channel.send("<:dmt:733931789243908136> " + "**Horde Dire Maul Tribute**" + " <:dmt:733931789243908136>" + "\n**On:** $Layer" + "\n**By:** $Buffer");
+  if(message.content.toUpperCase() == "!DMT-SUMMON-H") {
+    message.channel.send("<:dmt:733931789243908136> " + "**Horde Dire Maul Tribute**" + "\n**On:** $Layer" + "\n**By:** $Buffer");
   }
-  if(message.content === "!DMF-SUMMON-H") {
-    message.channel.send("<:dmf:733931791798370335> " + "**Horde Darkmoon Faire**" + " <:dmf:733931791798370335>" + "\n**On:** $Layer" + "\n**By:** $Buffer");
+  if(message.content.toUpperCase() == "!DMF-SUMMON-H") {
+    message.channel.send("<:dmf:733931791798370335> " + "**Horde Darkmoon Faire**" + "\n**On:** $Layer" + "\n**By:** $Buffer");
   }
-  if(message.content === "!BB-SUMMON-H") {
-    message.channel.send("<:zg:733931791802564628> " + "**Horde Booty Bay Summon**" + " <:zg:733931791802564628>" + "\n**On:** $Layer" + "\n**By:** $Buffer");
+  if(message.content.toUpperCase() == "!BB-SUMMON-H") {
+    message.channel.send("<:zg:733931791802564628> " + "**Horde Booty Bay Summon**" + "\n**On:** $Layer" + "\n**By:** $Buffer");
   }
-  if(message.content === "!SF-SUMMON-H") {
-    message.channel.send("<:songflower:733931791643181137> " + "**Horde Songflower Summon**" + " <:songflower:733931791643181137>" + "\n**On:** $Layer" + "\n**At:** $Time" + "\n**By:** $Buffer");
+  if(message.content.toUpperCase() == "!SF-SUMMON-H") {
+    message.channel.send("<:songflower:733931791643181137> " + "**Horde Songflower Summon**" + "\n**On:** $Layer" + "\n**At:** $Time" + "\n**By:** $Buffer");
   }
-  if(message.content === "!LAYER-H") {
-    message.channel.send("<:vanish:734106867227164702> " + "**Horde Layering**" + " <:vanish:734106867227164702>" + "\n**On:** $Layer" + "\n**By:** $Buffer");
+  if(message.content.toUpperCase() == "!LAYER-H") {
+    message.channel.send("<:vanish:734106867227164702> " + "**Horde Layering**" + "\n**On:** $Layer" + "\n**By:** $Buffer");
   }
-  if(message.content === "!PORT-H") {
-    message.channel.send("<:orgport:734106867160055843> " + "**Horde Ports**" + " <:orgport:734106867160055843>" + "\n**On:** $Layer"+ "\n**Cost:** $Cost" + "\n**By:** $Buffer");
+  if(message.content.toUpperCase() == "!PORT-H") {
+    message.channel.send("<:orgport:734106867160055843> " + "**Horde Ports**" + "\n**On:** $Layer"+ "\n**Cost:** $Cost" + "\n**By:** $Buffer");
   }
-  if(message.content === "!HORDE-ALL") {
-    message.channel.send("<:dmt:733931789243908136> " + "**All Upcoming Horde Buffs**" + " <:dmt:733931789243908136>" + "\n$ONY-H" + "\n$NEF-H" + "\n$SF-H" + "\n$WCB-H" + "\n$DMT-SUMMON-H" + "\n$BB-SUMMON-H" + "\n$SF-SUMMON-H" + "\n$LAYER-H" + "\n$PORT-H" + "\n$ZG");
+  if(message.content.toUpperCase() == "!HORDE-ALL") {
+    message.channel.send("<:dmt:733931789243908136> " + "**All Upcoming Horde Buffs**" + "\n<:ony:733931792297492490> $ONY-H" + "\n<:nef:733931791538192415> $NEF-H" + "\n<:songflower:733931791643181137> $SF-H" + "\n<:wcb:733931791567683614> $WCB-H" + "\n<:dmt:733931789243908136> $DMT-SUMMON-H" +"\n<:dmf:733931791798370335> $DMF-SUMMON" + "\n<:zg:733931791802564628> $BB-SUMMON-H" + "\n<:songflower:733931791643181137> $SF-SUMMON-H" + "\n<:vanish:734106867227164702> $LAYER-H" + "\n<:orgport:734106867160055843> $PORT-H" + "\n<:zg:733931791802564628> $ZG");
   }
 });
 

6c94b0f66feac90a746a0911810341459acb23d0
Justin <justinjrestivo@gmail.com>
2020-07-19 00:57:42 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -225,42 +225,6 @@ client.on("message", (message) => {
   " + "\n<:orgport:734106867160055843> $PORT-H\
   " + "\n<:zg:733931791802564628> $ZG");
   }
-
-//message.channel.send({embed: {
-//  color: 3447003,
-//  description: "A very simple Embed!"
-//}});
-
-//message.channel.send({embed: {
-//    color: 3447003,
-//    author: {
-//      name: client.user.username,
-//      icon_url: client.user.avatarURL
-//    },
-//    title: "This is an embed",
-//    url: "http://google.com",
-//    description: "This is a test embed to showcase what they look like and what they can do.",
-//    fields: [{
-//        name: "Fields",
-//        value: "They can have different fields with small headlines."
-//      },
-//      {
-//        name: "Masked links",
-//        value: "You can put [masked links](http://google.com) inside of rich embeds."
-//      },
-//      {
-//        name: "Markdown",
-//        value: "You can put all the *usual* **__Markdown__** inside of them."
-//      }
-//    ],
-//    timestamp: new Date(),
-//    footer: {
-//      icon_url: client.user.avatarURL,
-//      text: "© Example"
-//    }
-//  }
-//});
-
 });
 
 //Log in and start
